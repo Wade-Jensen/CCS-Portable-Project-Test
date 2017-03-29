fc705a5be1814177d211f898537a33ce30720399
Wade Jensen <wadejensenwork@gmail.com>
2017-03-30 00:40:26 +1100

    Project rename and added instrustions to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# CCS-Portable-Project-Test
-A template for a portable project with TI Code COmposer Studio
+# EGH456
+Project for EGH456. Embedded Electronics with Tiva Board
+
+######## Installation
+
+##### Installation
+Download the TI Examples folder from Blackboard, unzip it, and place
+the TivaWare_C_Series-2.1.3.156 folder in the root directory of the
+repository. There is a .gitignore rule setup so the folder will not
+be checked into source control.
+
+Window -> Preferences -> Code Composer Studio -> Products -> RTSC
+Remove all of the existing packages underneath Products and Repositories tab,
+these will be things like SYS/BIOS 6.4 and XCD Tools 3.32 or 3.50
+
+Code Composer Studio -> Build -> Variables
+Add a new variable called "TI_LIB_ROOT", the value will be the file path
+to the TivaWare_C_Series-2.1.3.156 folder you just unzipped.
+
+Download:
+
+**NDK 2.24.02.31**  
+<http://software-dl.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/ndk/index.html>  
+Unzip into TI install path, usually "C:\ti" on Windows
+
+**SYS/BIOS 6.x**  
+<http://software-dl.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/bios/index.html>  
+Run the exe and point it to the TI install path
+
+**TI-RTOS 2.12.01.33**  
+<http://software-dl.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/tirtos/index.html?DCMP=ep-mcu-rtos-en&HQS=ep-mcu-rtos-conw-20150521-sw-en>  
+Run the exe and point it to the TI install path
+
+Import the ProjectTemplate_RTOS project available on Blackboard.  
+Right click on the project within the Project Explorer pane.  
+Select "Show Build Settings" -> General
+
+Connection -> Stellaris In-Circuit Debug Interface
+Compiler version -> GNU v4.8.4 (Linaro) 
+	- or 4.9.3 if the other is not available
+
+Build -> GNU Linker -> Libraries
+Add a library search path. Path is: "${TI_LIB_ROOT}"
+which is the variable you setup earlier.
+
+Restart CCS
+
+Try and build the project.  
+*Good luck!*
+
+If the project builds successfully you may want to change its name from the sample project name.  
+Right click on the project, select rename and enter the new project name. The project folder
+will be renamed also when this is done.
+
+
+
+
+
+
--- Testing_Station_RTOS/Debug/subdir_rules.mk
+++ Testing_Station_RTOS/Debug/subdir_rules.mk
@@ -5,24 +5,24 @@
 SHELL = cmd.exe
 
 # Each subdirectory must supply rules for building sources it contributes
-build-1588942241:
-	@$(MAKE) -Onone -f subdir_rules.mk build-1588942241-inproc
+build-454593023:
+	@$(MAKE) -Onone -f subdir_rules.mk build-454593023-inproc
 
-build-1588942241-inproc: ../app.cfg
+build-454593023-inproc: ../app.cfg
 	@echo 'Building file: $<'
 	@echo 'Invoking: XDCtools'
 	"C:/ti/xdctools_3_31_00_24_core/xs" --xdcpath="C:/ti/tirtos_tivac_2_12_01_33/packages;C:/ti/tirtos_tivac_2_12_01_33/products/bios_6_41_04_54/packages;C:/ti/tirtos_tivac_2_12_01_33/products/ndk_2_24_02_31/packages;C:/ti/tirtos_tivac_2_12_01_33/products/uia_2_00_02_39/packages;C:/ti/ccsv7/ccs_base;" xdc.tools.configuro -o configPkg -t gnu.targets.arm.M4F -p ti.platforms.tiva:TM4C129XNCZAD -r release -c "C:/ti/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-configPkg/linker.cmd: build-1588942241 ../app.cfg
-configPkg/compiler.opt: build-1588942241
-configPkg/: build-1588942241
+configPkg/linker.cmd: build-454593023 ../app.cfg
+configPkg/compiler.opt: build-454593023
+configPkg/: build-454593023
 
 %.o: ../%.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU Compiler'
-	"C:/ti/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-gcc.exe" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DPART_TM4C129XNCZAD -I"C:/ti/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include" -I"C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Assignment/ProjectTemplate_RTOS" -I"C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c" -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -MD -std=c99 -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o"$@" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi-gcc.exe" -c -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DPART_TM4C129XNCZAD -I"C:/ti/ccsv7/tools/compiler/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include" -I"C:/Users/WadeJensen/Dropbox/01_EN40/YEAR_4_SEM_1/EGH456/Assignment/Testing_Station_RTOS" -I"C:/ti/tirtos_tivac_2_12_01_33/products/TivaWare_C_Series-2.1.0.12573c" -ffunction-sections -fdata-sections -g -gdwarf-3 -gstrict-dwarf -Wall -MD -std=c99 -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o"$@" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
